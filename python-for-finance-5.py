ffd2290d4c682c1a8644f1eec9a977d635048a59
JayGwod <guodejie001@126.com>
2019-10-17 02:59:59 +0800

    Add p.5

---
+++ python-for-finance-5.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2019 GUO DEJIE <dejie.guo@gmail.com>
+#
+# Distributed under terms of the MIT license.
+"""
+Automating getting the S&P 500 list - Python Programming for Finance p.5
+"""
+import pickle
+
+import bs4 as bs
+import requests
+
+
+def save_sp500_tickers():
+    resp = requests.get(
+        'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+    soup = bs.BeautifulSoup(resp.text, 'lxml')
+    table = soup.find('table', {'class': 'wikitable sortable'})
+    tickers = []
+
+    for row in table.findAll('tr')[1:]:
+        ticker = row.findAll('td')[0].text
+        tickers.append(ticker)
+
+    with open('sp500tickers.pickle', 'wb') as f:
+        pickle.dump(tickers, f)
+
+    print(tickers)
+
+    return tickers
+
+
+save_sp500_tickers()
