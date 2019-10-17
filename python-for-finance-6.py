64907d64e5469c4c23ea2129a685eed5e981c3a4
JayGwod <guodejie001@126.com>
2019-10-17 19:30:39 +0800

    Add lesson p.6

---
+++ python-for-finance-6.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2019 GUO DEJIE <dejie.guo@gmail.com>
+#
+# Distributed under terms of the MIT license.
+"""
+Getting all company pricing data in the S&P 500 - Python Programming for Finance p.6
+"""
+import pickle
+import os
+import bs4 as bs
+import requests
+import datetime as dt
+import pandas as pd
+import pandas_datareader.data as web
+
+def save_sp500_tickers():
+    resp = requests.get(
+        'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+    soup = bs.BeautifulSoup(resp.text, 'lxml')
+    table = soup.find('table', {'class': 'wikitable sortable'})
+    tickers = []
+
+    for row in table.findAll('tr')[1:]:
+        ticker = row.findAll('td')[0].text
+        tickers.append(ticker.strip())
+
+    with open('sp500tickers.pickle', 'wb') as f:
+        pickle.dump(tickers, f)
+
+    print(tickers)
+
+    return tickers
+
+def get_data_from_yahoo(reload_sp500=False):
+
+    if reload_sp500:
+        tickers = save_sp500_tickers()
+    else:
+        with open('sp500tickers.pickle', 'rb') as f:
+            tickers = pickle.load(f)
+
+    if not os.path.exists('stock_dfs'):
+        os.makedirs('stock_dfs')
+
+    start = dt.datetime(2000,1,1)
+    end = dt.datetime(2016,12,31)
+
+    for ticker in tickers:
+        print(ticker)
+        if not os.path.exists('stock_dfs/{}.csv'.format(ticker)):
+            df = web.DataReader(ticker, 'yahoo', start, end)
+            df.to_csv('stock_dfs/{}.csv'.format(ticker))
+        else:
+            print('Already have {}'.format(ticker))
+
+get_data_from_yahoo(True)
